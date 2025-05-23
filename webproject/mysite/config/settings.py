1d1d47b893befefa053c268a5c3c295770afdb04
shwnsgur2007519 <shwnsgur2007519@gmail.com>
2025-05-24 00:46:55 +0900

    배포

--- webproject/mysite/config/settings.py
+++ webproject/mysite/config/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'Calendar' / 'static',
 ]
